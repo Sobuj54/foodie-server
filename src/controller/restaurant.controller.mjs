a44068df4736ab4d367229d3d5ca3e40a85c6306
soubj <sobujahmed124@gmail.com>
2024-03-23 23:28:33 +0600

    added createRestaurant

--- src/controller/restaurant.controller.mjs
+++ src/controller/restaurant.controller.mjs
@@ -1,3 +1,29 @@
-const createMyRestaurant = async (req, res) => {};
+import mongoose from "mongoose";
+import { Restaurant } from "../models/restaurant.model.mjs";
+import uploadOnCloudinary from "../utils/cloudinary.mjs";
+
+const createMyRestaurant = async (req, res) => {
+  try {
+    const existingRestaurant = await Restaurant.findOne({ user: req.userId });
+    if (existingRestaurant) {
+      return res
+        .status(409)
+        .json({ message: "User restaurant already exists." });
+    }
+
+    const imgPath = req.file.path;
+    const uploadResponse = await uploadOnCloudinary(imgPath);
+
+    const restaurant = new Restaurant(req.body);
+    restaurant.image = uploadResponse.url;
+    restaurant.user = new mongoose.Types.ObjectId(req.userId);
+    await restaurant.save();
+
+    return res.status(201).json(restaurant);
+  } catch (error) {
+    console.log("create restaurant err :", error);
+    return res.status(500).json({ message: "Something went wrong." });
+  }
+};
 
 export { createMyRestaurant };
--- src/routes/restaurant.routes.mjs
+++ src/routes/restaurant.routes.mjs
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware.mjs";
 import { createMyRestaurant } from "../controller/restaurant.controller.mjs";
+import { jwtCheck, jwtParse } from "../middlewares/auth.middleware.mjs";
 
 const router = Router();
 
-router.post("/", upload.single("image"), createMyRestaurant);
+router.post(
+  "/create",
+  jwtCheck,
+  jwtParse,
+  upload.single("image"),
+  createMyRestaurant
+);
 
 export default router;
